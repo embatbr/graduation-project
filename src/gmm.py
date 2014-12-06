9b7267e45be7579a6eb177e6216a25ccddea1de6
Eduardo Tenorio <embatbr@gmail.com>
2014-12-05 22:42:38 -0300

    Inserting docs in module 'gmm'.
    Signed-off-by: Eduardo Tenorio <embatbr@gmail.com>

--- src/gmm.py
+++ src/gmm.py
@@ -32,6 +32,13 @@ def gaussian(features, means=np.array([0]), covariances=np.array([[1]])):
     return (cte * math.exp(power))
 
 def create_gmm(M, D):
+    """Creates a GMM.
+
+    @param M: number of mixtures.
+    @param D: size of features (used to the means and covariances).
+
+    @returns: an untrained GMM.
+    """
     weights = np.array([random.uniform(1, 10) for _ in range(M)])
     weights = (1/np.sum(weights)) * weights
 
@@ -49,12 +56,32 @@ def create_gmm(M, D):
     return gmm
 
 def eval_gmm(gmm, features):
+    """Feeds the GMM with the given features.
+
+    @param gmm: the GMM used (a list of (weight, means, covariances));
+    @param features: a Dx1 vector of features.
+
+    @returns: the weighted sum of gaussians of gmm.
+    """
     prob = 0
     for mixture in gmm:
         (weight, means, covariances) = mixture
         prob = prob + weight*gaussian(features, means, covariances)
 
     return prob
+
+def eval_avg_gmm(gmms, mfccs):
+    D = len(mfccs[:, 0])
+    logprob = 0
+
+    for (gmm, features) in zip(gmms, mfccs.T):
+        prob = eval_gmm(gmm, features)
+        logprob = logprob + math.log(prob, 10)
+
+    T = len(mfccs[0])
+    logprob = (1/T) * logprob
+
+    return logprob
 
 #TODO criar funcao para gerar o UBM-GMM e treiná-lo
 
@@ -64,6 +91,7 @@ if __name__ == '__main__':
     import random
     from useful import CORPORA_DIR, FEATURES_DIR
 
+    #Showing the 1st MFCC (index ZERO) over time
     mfccs = corpus.read_features(13, 0, 0.97, 'enroll_1', 'f00', 1)
     T = len(mfccs[0])
     fig = plt.figure()
@@ -73,10 +101,11 @@ if __name__ == '__main__':
     plt.xlabel('frame')
     plt.ylabel('feature value')
 
+    #Showing random D-featured gaussian over time
     frames = np.linspace(1, T, T)
     value = list()
     for features in mfccs.T:
-        #features = features[0 : 1]  #D = 1 (only 1 feature)
+        #features = features[0 : 1]  #D = 1 (only 1st feature)
         D = len(features)
         means = np.array([random.uniform(5, 10) for _ in range(D)])
         covariances = np.zeros((D, D))
@@ -92,6 +121,7 @@ if __name__ == '__main__':
     plt.ylabel('feature value')
     plt.plot(frames, value) #figure 2
 
+    #Evaluating a GMM with M mixtures for a Dx1 feature vector
     M = 32
     features = mfccs.T[0]
     D = len(features)
@@ -99,14 +129,20 @@ if __name__ == '__main__':
     prob = eval_gmm(gmm, features)
     print(prob)
 
+    #Evaluating one GMMs with M mixtures for a Dx1 feature vector for each frame
     probs = list()
+    logprob = 0
     for features in mfccs.T:
         #features = features[0 : 1]  #D = 1 (only 1 feature)
         D = len(features)
         gmm = create_gmm(M, D)
         prob = eval_gmm(gmm, features)
+        logprob = logprob + math.log(prob, 10)
         print(prob)
         probs.append(prob)
+    T = len(mfccs[0])
+    logprob = (1/T) * logprob
+    print('T =', T, 'logprob =', logprob)
     probs = np.array(probs)
     fig = plt.figure()
     fig.suptitle('GMMs \n1 <= frame <= %d' % T)
@@ -115,4 +151,9 @@ if __name__ == '__main__':
     plt.ylabel('prob')
     plt.plot(frames, probs) #figure 3
 
-    plt.show()
+    gmms = [create_gmm(M, len(features)) for features in mfccs.T]
+    print('T =', len(gmms), 'M =', len(gmms[0]))
+    logprob = eval_avg_gmm(gmms, mfccs)
+    print('logprob =', logprob)
+
+    #plt.show()
