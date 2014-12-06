9a1c8cac8c0abf8621b397304ee442aad9fe9761
Eduardo Tenorio <embatbr@gmail.com>
2014-12-06 02:14:54 -0300

    Added function 'testplot' in module 'useful'.
    Signed-off-by: Eduardo Tenorio <embatbr@gmail.com>

--- src/useful.py
+++ src/useful.py
@@ -2,6 +2,33 @@
 """
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 BASES_DIR = '../bases/'
 CORPORA_DIR = '%scorpora/' % BASES_DIR
 FEATURES_DIR = '%sfeatures/' % BASES_DIR
+
+
+def testplot(x, y, suptitle='', xlabel='', ylabel=''):
+    """Creates a Matplotlib figure and plots the @param y related to @param x.
+
+    @param x: a numpy array.
+    @param y: a numpy array.
+    @param suptitle: the title of the figure.
+    @param xlabel: the label of the x axis.
+    @param ylabel: the label of the y axis.
+    @param xlim: the limits of the x axis.
+    @param ylim: the limits of the y axis.
+    """
+    fig = plt.figure()
+    fig.suptitle(suptitle)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    plt.xlim([x[0], len(x)])
+    plt.ylim([np.amin(y), np.amax(y)])
+
+    plt.grid(True)
+    plt.plot(x, y)
