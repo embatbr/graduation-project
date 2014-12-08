89f38f35b96aac14692dc0c5cf45b96d01ea3f0e
Eduardo Tenorio <embatbr@gmail.com>
2014-12-08 02:01:47 -0300

    New references added.
    Signed-off-by: Eduardo Tenorio <embatbr@gmail.com>

--- docs/paper/runtg.sh
+++ docs/paper/runtg.sh
@@ -1,3 +1,7 @@
+#rm *.aux *.lof *.log *.lot *.toc
+#pdflatex --shell-escape embat-tg.tex
+#rm *.aux *.lof *.log *.lot *.toc
+
 rm *.aux *.lof *.log *.lot *.toc
-pdflatex --shell-escape embat-tg.tex
+pdflatex feature-extraction.tex
 rm *.aux *.lof *.log *.lot *.toc
