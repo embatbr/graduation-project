c42ead1e358204701eb2b96fe369fc220b693d71
Eduardo Tenorio <embatbr@gmail.com>
2014-12-08 04:25:06 -0300

    MACRO for the image repository of module 'features_test' and function 'testmultiplot', both in module 'useful'.
    Signed-off-by: Eduardo Tenorio <embatbr@gmail.com>

--- src/useful.py
+++ src/useful.py
@@ -11,6 +11,7 @@ CORPORA_DIR = '%scorpora/' % BASES_DIR
 FEATURES_DIR = '%sfeatures/' % BASES_DIR
 IMAGES_DIR = '../docs/paper/images/'
 IMAGES_SIGPROC_DIR = '%ssigproc' % IMAGES_DIR
+IMAGES_FEATURES_DIR = '%sfeatures' % IMAGES_DIR
 
 
 def testplot(x, y, suptitle='', xlabel='', ylabel='', filename=None, fill=False,
@@ -22,7 +23,6 @@ def testplot(x, y, suptitle='', xlabel='', ylabel='', filename=None, fill=False,
     @param suptitle: the title of the figure.
     @param xlabel: the label of the x axis.
     @param ylabel: the label of the y axis.
-    @param xlim: the limits of the x axis.
     @param ylim: the limits of the y axis.
     """
     plt.clf()
@@ -30,10 +30,34 @@ def testplot(x, y, suptitle='', xlabel='', ylabel='', filename=None, fill=False,
     fig.suptitle(suptitle)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    #plt.xlim([x[0], x[-1]])
     plt.grid(True)
+
     if fill:
         plt.fill_between(x, y, color=color)
     plt.plot(x, y, color=color)
+
+    if not filename is None:
+        plt.savefig('%s%s.png' % (IMAGES_DIR, filename))
+
+def testmultiplot(x, y, suptitle='', xlabel='', ylabel='', filename=None, color='blue'):
+    """Creates a Matplotlib figure and plots the @param y related to @param x.
+
+    @param x: a numpy array.
+    @param y: a numpy array of the same size of @param x.
+    @param suptitle: the title of the figure.
+    @param xlabel: the label of the x axis.
+    @param ylabel: the label of the y axis.
+    @param ylim: the limits of the y axis.
+    """
+    plt.clf()
+    fig = plt.gcf()
+    fig.suptitle(suptitle)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+
+    for i in range(len(y)):
+        plt.plot(x, y[i], color=color)
+
     if not filename is None:
         plt.savefig('%s%s.png' % (IMAGES_DIR, filename))
